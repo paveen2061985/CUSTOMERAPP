19b204eae108a4b15ea837cca381ef61caf2f7c7
Praveen <praveenreddy206@gmail.com>
2021-11-03 03:53:19 +0530

    Routing Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerSupplierComponent } from './customer-supplier/customer-supplier.component';
+import { CustomerComponent } from './customer/customer.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:HomeComponent},
+  {path:"supplier", component: CustomerSupplierComponent},
+  {path: "customer", component: CustomerComponent},
+  {path:'', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
